1ca664a6a4d514377ca46510e8817df5961e7bdd
ShawnTownsend <stitans28@gmail.com>
2021-11-08 15:14:50 +0000

    Fixed

--- index.js
+++ index.js
@@ -4,15 +4,15 @@
 // 3. Iterate over the array using a for loop to get every element in the array.
 // 4. In the for loop: add every element to the `average` variable.
 // 5. Use array.length() to find how many elements are in the array.
-// 6. After the loop: Return the value of `average`.
+// 6. After the loop: Divide `average` by the length of the array.
 
 
 
 //Question 1
 function averageInArray(array) {
-    let sum = 0;
+    let average = 0;
     for (let i = 0; i < array.length; i++) {
-        sum = sum + array[i];
+        average = average + array[i];
     }
-    return sum / array.length;
+    return average / array.length;
 }
